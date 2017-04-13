4f8937b2a746d113741ae620babbb9f9449df892
luisreys <theluisitorey@usal.es>
2017-04-13 12:24:50 +0300

    New Classes and DB functions
    I've created two new classes: User and Admin, which extends to User.
    When you do login, all your information goes to one of those classes,
    depending of your state: admin or normal user.
    Every class has its owns methods:
    User: - AddPerson
    - UpdatePerson
    - DeletePerson
    Admin: inheritance of User and:
    - AddUser
    - UpdatePerson
    - DeletePerson
    
    All this functions have its parallel in the DB file.

--- controller/db.php
+++ controller/db.php
@@ -4,7 +4,7 @@
     $mysqli = new mysqli("localhost", "root", "", "mvcex");
     if ($mysqli->connect_errno) {
       echo "Error #: " . $mysqli->connect_error;
-      exit;
+      return -1;
     }
     return $mysqli;
   }
@@ -52,4 +52,169 @@
     $mysqli->close();
   }
 
+  /*
+  * From here there are functions for Class methods.
+  * First: addPerson, updatePerson and deletePerson
+  */
+
+
+  /*
+  *   Add a person to the DB.
+  *   Return: 0 - Everything is okay
+  *          -1 - This person already exist, then, impossible to add again
+  *          -2 - Error in stmt->execute()
+  *          -3 - Error in mysqli->prepare
+  *          -4 - database connect error
+  */
+  function addPersonDB($id, $fname, $sname){
+    $mysqli = connectdb();
+    if ($mysqli == -1) {
+      return -4;
+    }
+
+    // First I'm gonna check if this person already exist in the db
+    if ($stmt = $mysqli->prepare("SELECT * FROM servside2017_persons WHERE id=?")) {
+      $stmt->bind_param("i", $id);
+      if (!$stmt->execute()) {
+        $mysqli->close();
+        return -2;
+      }
+      $result = $stmt->get_result();
+
+      if (!$result->num_rows ) {
+        // No result, then I can add the new person.
+
+        if ($stmt = $mysqli->prepare("INSERT INTO servside2017_persons (id, fname, sname) VALUES (?, ?, ?)")) {
+          $stmt->bind_param("iss", $id, $fname, $sname);
+          if ($stmt->execute()){
+            // Inserted
+            $mysqli->close();
+            return 0;
+          }else {
+            // No inserted
+            $mysqli->close();
+            return -2;
+          }
+        }else {
+          $mysqli->close();
+          return -3;
+        }
+      }else {
+        // This person already exists
+        $mysqli->close();
+        return -1;
+      }
+    }else {
+      $mysqli->close();
+      return -3;
+    }
+  }
+
+/*
+*   Update a person on the DB.
+*   Return: 0 - Everything is okay
+*          -1 - Id doesn't exist in the DB
+*          -2 - Error in stmt->execute()
+*          -3 - Error in mysqli->prepare
+*          -4 - database connect error
+*/
+  function updatePersonDB($id, $fname, $sname){
+    $mysqli = connectdb();
+    if ($mysqli == -1) {
+      return -4;
+    }
+
+    // First I'm gonna check if this person exist in the db
+    if ($stmt = $mysqli->prepare("SELECT * FROM servside2017_persons WHERE id=?")) {
+      $stmt->bind_param("i", $id);
+      if (!$stmt->execute()) {
+        $mysqli->close();
+        return -2;
+      }
+      $result = $stmt->get_result();
+
+      if (!$result->num_rows ) {
+        // No result, then I can't update it.
+        return -1;
+      }else {
+        // This person exists, then I can update the table.
+        if ($stmt = $mysqli->prepare("UPDATE servside2017_persons SET id=?, fname=?, sname=? WHERE id=?")) {
+          $stmt->bind_param("issi", $id, $fname, $sname, $id);
+          if ($stmt->execute()){
+            // Updated
+            $mysqli->close();
+            return 0;
+          }else {
+            // No Updated
+            $mysqli->close();
+            return -2;
+          }
+        }else {
+          //Error in prepare sentence
+          $mysqli->close();
+          return -3;
+        }
+      }
+    }else {
+      //Error in prepare sentence
+      $mysqli->close();
+      return -3;
+    }
+
+    /*
+    *   Delete a person to the DB.
+    *   Return: 0 - Everything is okay
+    *          -1 - Id doesn't exist in the DB
+    *          -2 - Error in stmt->execute()
+    *          -3 - Error in mysqli->prepare
+    *          -4 - database connect error
+    */
+  }
+  function deletePersonDB($id){
+    $mysqli = connectdb();
+    if ($mysqli == -1) {
+      return -4;
+    }
+
+/// First I'm gonna check if there are a person with this id, if it does, i can delete it
+/// otherwise i can't delete anything.
+    if ($stmt = $mysqli->prepare("SELECT * FROM servside2017_persons WHERE id=?")) {
+      $stmt->bind_param("i", $id);
+      if (!$stmt->execute()) {
+        //Doesn't work
+        $mysqli->close();
+        return -2;
+      }
+      $result = $stmt->get_result();
+
+      //I'm gonna check if there are some row
+      if (!$result->num_rows) {
+        //This id doesn't exist in the database. Then I can't remove it
+        $mysqli->close();
+        return -1;
+      }else {
+        //This id exist in the database, then, I can delete it
+
+        if ($stmt = $mysqli->prepare("DELETE * FROM servside2017_persons WHERE id=?")) {
+          $stmt->bind_param("i", $id);
+          if (!$stmt->execute()) {
+            //Something was wrong
+            $mysqli->close();
+            return -2;
+          }else {
+            //Deleted!
+            $mysqli->close();
+            return 0;
+          }
+        }else {
+          //MYSQLI couldn't prepare it
+          $mysqli->close();
+          return -3;
+        }
+      }
+    }else {
+      $mysqli->close();
+      return -3;
+    }
+  }
  ?>
---
+++ user.php
@@ -0,0 +1,52 @@
+<?php
+  include_once "db.php";
+  /**
+   *  Class User which saves all the information about the user who logged in.
+   *  It also give to the user the chance to user some methods only for users.
+   */
+  class user{
+    var $username = "";
+    var $pri;
+    var $id;
+    var $description = "";
+    function __construct($username, $pri, $id, $description)
+    {
+      $this->username = $username;
+      $this->pri = $pri;
+      $this->id = $id;
+      $this->description = $description;
+    }
+
+    /*
+    *   Add, Update and remove person.
+    *   Return: 0 - Everything is okay
+    *          -1 - Id doesn't exist in the DB
+    *          -2 - Error in stmt->execute()
+    *          -3 - Error in mysqli->prepare
+    *          -4 - database connect error
+    */
+    function addPerson($id, $fname, $sname){
+      $ret = addPersonDB($id, $fname, $sname);
+    }
+    function updatePerson($id, $fname, $sname){
+
+    }
+    function deletePerson($id){
+
+    }
+  }
+
+
+  /**
+   *
+   */
+  class admin extends user
+  {
+
+    function __construct(argument)
+    {
+      # code...
+    }
+  }
+
+ ?>
